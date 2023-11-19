2b307a9deedd7eebec6b47ff8e28b0ba92cc40e1
Steven Pak <spak9@gmu.edu>
2023-11-18 19:04:56 -0500

    more ios dev

--- ios/Xylophone-iOS13-master/Xylophone/ViewController.swift
+++ ios/Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -1,51 +1,24 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let letter: String = sender.titleLabel!.text!
+        print("Pressed \(letter)")
+        playSound(letter: letter)
+    }
     
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
-        let player = try! AVAudioPlayer(contentsOf: url!)
-        print(player)
+    func playSound(letter: String) {
+        let url = Bundle.main.url(forResource: letter, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-//        guard let url = Bundle.main.url(forResource: "C", withExtension: ".wav") else { return }
-//
-//        do {
-//            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-//            print(1)
-//            try AVAudioSession.sharedInstance().setActive(true)
-//            print("adsf")
-//
-//            let player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-//
-//            player.play()
-//
-//        } catch let error {
-//            print(error)
-//        }
+                
     }
-
-
-    @IBAction func keyCPressed(_ sender: UIButton) {
-        print("C")
-        playSound()
-    }
-    
-    @IBAction func keyDPressed(_ sender: Any) {
-        print("'D' Pressed")
-    }
-    
 }
-
